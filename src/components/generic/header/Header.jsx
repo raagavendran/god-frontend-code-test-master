4a275c73a43949762fe55761d1582428620cb13e
Raaga <raagavendran@gmail.com>
2024-08-25 12:09:23 +0530

    Added Home Route

--- src/components/generic/header/Header.jsx
+++ src/components/generic/header/Header.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Headerstyles from "./Header.module.scss";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+
+  // Home page route on logo click
+  const redirectHome = () => {
+    router.push(`/`);
+  };
   return (
     <header className={Headerstyles.header}>
-      <h1 className={Headerstyles.companyName}>VOLVO</h1>
+      <h1 className={Headerstyles.companyName} onClick={redirectHome}>
+        VOLVO
+      </h1>
     </header>
   );
 };
